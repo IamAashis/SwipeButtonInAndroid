9799ca06bfa16b334a4ea88d1417f46c3a5a6ea9
Aashis <thapaaashis20@gmail.com>
2023-07-28 00:12:07 +0545

    Added button animation

--- app/src/main/java/com/android/swipebuttonandroid/MainActivity.kt
+++ app/src/main/java/com/android/swipebuttonandroid/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.android.swipebuttonandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import java.security.AccessController.getContext
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SwipeButton.OnSwipeButtonStateChangeListener {
+
+    lateinit var swipe_btn: SwipeButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        swipe_btn = findViewById(R.id.swipe_btn)
+        swipe_btn.setOnSwipeButtonStateChangeListener(this);
+    }
+
+    override fun onButtonStateChange(active: Boolean) {
+        Toast.makeText(this, "Completed", Toast.LENGTH_SHORT).show()
     }
 }
